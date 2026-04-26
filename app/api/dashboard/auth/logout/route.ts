7620545067599edc4ee7e238b599c0c0d3071afa
s0fy4n002 <s0fy4n002@gmail.com>
2026-04-26 13:15:10 +0700

    clear

--- app/api/dashboard/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function POST() {
-  const response = NextResponse.json({ message: 'Logout berhasil' })
-  
-  response.cookies.delete('auth-token')
-  
-  return response
-}
